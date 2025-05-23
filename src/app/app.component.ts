03ba430d0406f286b62c70f23e8468c1b33fd64a
Romain <romain.1116@outlook.com>
2025-05-23 10:40:49 +0200

    Maj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,28 @@
-import {Component, computed, inject, signal} from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {CustomSidenavComponent} from './components/custom-sidenav/custom-sidenav.component';
-import {AuthService} from './services/auth.service';
-import {MatTooltip} from '@angular/material/tooltip';
-
+import { Component, computed, inject, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { CustomSidenavComponent } from './components/custom-sidenav/custom-sidenav.component';
+import { AuthService } from './services/auth.service';
+import { MatTooltip } from '@angular/material/tooltip';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import {NgIf} from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet,
+    imports: [
+        RouterOutlet,
         RouterLink,
-        MatToolbarModule, MatButtonModule,
-        MatIconModule, MatSidenavModule, CustomSidenavComponent, MatTooltip,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSidenavModule,
+        CustomSidenavComponent,
+        MatTooltip,
+        NgIf
     ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
@@ -23,14 +30,30 @@ import {MatTooltip} from '@angular/material/tooltip';
 export class AppComponent {
     title = 'front-angular';
     collapsed = signal(false);
-    sidenavWidth = computed(() => (this.auth.connected ? (this.collapsed() ? "65px" : "250px") : "0px"));
-    auth = inject(AuthService)
+    darkMode = signal(false);
+
+    auth = inject(AuthService);
+    overlay = inject(OverlayContainer);
+
+    sidenavWidth = computed(() =>
+        this.auth.connected ? (this.collapsed() ? "65px" : "250px") : "0px"
+    );
 
     onSignOut() {
-        localStorage.removeItem("jwt")
-        this.auth.connected = false
-
+        localStorage.removeItem("jwt");
+        this.auth.connected = false;
     }
 
-
+    toggleDarkMode() {
+        this.darkMode.update((v) => !v);
+        const isDark = this.darkMode();
+        const html = document.documentElement;
+        if (isDark) {
+            html.classList.add('dark-theme');
+            this.overlay.getContainerElement().classList.add('dark-theme');
+        } else {
+            html.classList.remove('dark-theme');
+            this.overlay.getContainerElement().classList.remove('dark-theme');
+        }
+    }
 }
