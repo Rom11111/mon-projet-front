b2ae6ae3d521ae88e691ebd9395be08ffe19bf18
Rom11111 <romain.1116@outlook.com>
2025-08-02 14:33:29 +0200

    Ajout de l'enum `UserStatus` et modification de l'interface `User` pour utiliser ce type

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,44 @@ import { ClientLayoutComponent } from './layouts/client-layout/client-layout.com
 import {notLoggedGuard} from './guards/not-logged.guard';
 import {loggedGuard} from './guards/logged.guard';
 
-
 export const routes: Routes = [
+    // 🌐 ZONE PUBLIQUE
     {
         path: '',
         component: LoginLayoutComponent,
         children: [
-            { path: 'login', loadComponent: () => import('./pages/auth/login/login.component').then(m => m.LoginComponent), canActivate: [notLoggedGuard], data: { title: 'Connexion' } },
-            { path: 'signup', loadComponent: () => import('./pages/auth/signup/signup.component').then(m => m.SignupComponent), canActivate: [notLoggedGuard], data: { title: 'Inscription' } },
-            { path: 'forgot-password', loadComponent: () => import('./pages/auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent), canActivate: [notLoggedGuard], data: { title: 'Mot de passe oublié' } },
-            { path: 'reset-password', loadComponent: () => import('./pages/auth/reset-password/reset-password.component').then(m => m.ResetPasswordComponent), canActivate: [notLoggedGuard], data: { title: 'Réinitialisation du mot de passe' } },
-            { path: 'validate-mail/:token', loadComponent: () => import('./pages/email-validation/email-validation.component').then(m => m.EmailValidationComponent), data: { title: 'Validation de l\'email' } },
+            {path: 'login', loadComponent: () =>
+                    import('./pages/auth/login/login.component').then(m => m.LoginComponent),
+                canActivate: [notLoggedGuard],
+                data: { title: 'Connexion' }
+            },
+            {
+                path: 'signup',
+                loadComponent: () =>
+                    import('./pages/auth/signup/signup.component').then(m => m.SignupComponent),
+                canActivate: [notLoggedGuard],
+                data: { title: 'Inscription' }
+            },
+            {
+                path: 'forgot-password',
+                loadComponent: () =>
+                    import('./pages/auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent),
+                canActivate: [notLoggedGuard],
+                data: { title: 'Mot de passe oublié' }
+            },
+            {
+                path: 'reset-password',
+                loadComponent: () =>
+                    import('./pages/auth/reset-password/reset-password.component').then(m => m.ResetPasswordComponent),
+                canActivate: [notLoggedGuard],
+                data: { title: 'Réinitialisation du mot de passe' }
+            },
+            {
+                path: 'validate-mail/:token',
+                loadComponent: () =>
+                    import('./pages/email-validation/email-validation.component').then(m => m.EmailValidationComponent),
+                data: { title: 'Validation de l\'email' }
+            },
             { path: '', redirectTo: 'login', pathMatch: 'full' }
         ]
     },
@@ -27,6 +54,7 @@ export const routes: Routes = [
             { path: 'dashboard', loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent), data: { title: 'Tableau de bord' } },
             { path: 'equipments', loadComponent: () => import('./pages/products/equipments/equipments.component').then(m => m.EquipmentsComponent), data: { title: 'Équipements' } },
             { path: 'rental', loadComponent: () => import('./pages/rental/rental.component').then(m => m.RentalComponent), data: { title: 'Locations' } },
+            { path: 'my-rentals', loadComponent: () => import('./pages/rental/my-rentals.component').then(m => m.MyRentalsComponent), data: { title: 'My Rentals' }},
             { path: 'clients', loadComponent: () => import('./pages/clients/clients.component').then(m => m.ClientsComponent), data: { title: 'Clients' } },
             { path: 'team', loadComponent: () => import('./pages/team/team.component').then(m => m.TeamComponent), data: { title: 'Équipe' } },
             { path: 'contact', loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent), data: { title: 'Contact' } },
